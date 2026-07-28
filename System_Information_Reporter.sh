605788ada3a5fc86c440247ba2ea55cc99a7fa0b
Rishh369 <rishithachirumamilla@gmail.com>
2026-07-28 09:28:44 +0530

    Enhance output formatting with section headers
    Added section headers for better readability in the system information report.

--- System_Information_Reporter.sh
+++ System_Information_Reporter.sh
@@ -5,29 +5,43 @@ echo " System Information Report"
 echo "==============================="
 
 echo
+echo "--------------------"
 echo "Hostname:"
+echo "--------------------"
 hostname
 
 echo
+echo "--------------------"
 echo "Kernel Version:"
+echo "--------------------"
 uname -r
 
 echo
+echo "--------------------"
 echo "Operating System:"
+echo "--------------------"
 grep "^PRETTY_NAME" /etc/os-release
 
 echo
+echo "--------------------"
 echo "System Uptime:"
+echo "--------------------"
 uptime -p
 
 echo
+echo "--------------------"
 echo "CPU Information:"
+echo "--------------------"
 lscpu | grep "Model name"
 
 echo
+echo "--------------------"
 echo "Memory Usage:"
+echo "--------------------"
 free -h
 
 echo
+echo "--------------------"
 echo "Disk Utilization:"
+echo "--------------------"
 df -h
