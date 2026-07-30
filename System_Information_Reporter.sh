622202da4353a2a21f7aaaca62fac017ebeba0bc
Rishh369 <rishithachirumamilla@gmail.com>
2026-07-30 10:23:02 +0530

    added description of script

--- System_Information_Reporter.sh
+++ System_Information_Reporter.sh
@@ -1,4 +1,4 @@
-#This project shows the system information- 1)host name 2)Kernel Version 3)OS 4)System Uptime 5)CPU Info 6)Memory Usage
+#This script shows the system information- 1)host name 2)Kernel Version 3)OS 4)System Uptime 5)CPU Info 6)Memory Usage
 
 #!/bin/bash
 
